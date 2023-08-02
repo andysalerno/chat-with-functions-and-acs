fc7d4818657f6fb736903e322885133438e46dff
Andy Salerno <ansalern@microsoft.com>
2023-08-02 13:10:15 -0700

    Add missing class

---
+++ src/AzureCognitiveSearch/SemanticSearchResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace azureai.src.AzureCognitiveSearch;
+
+internal class SemanticSearchResponse
+{
+    [JsonPropertyName("likely_answer")]
+    public List<string>? LikelyAnswers { get; set; }
+
+    [JsonPropertyName("relevant_excerpts")]
+    public List<string> RelevantExcerpts { get; } = new List<string>();
+}
